116fbf4e062e79588bed9881559dfd50d585b5e6
Rob Willison <Rob@willison.co.uk>
2018-02-16 17:29:29 +0000

    tabs fix

--- main.py
+++ main.py
@@ -12,69 +12,69 @@ frontRightMotor = Motor(13, 18)
 motor4_enable = OutputDevice(25, initial_value=1)
 
 def pitch():
-    (mag_x, mag_y, mag_z) = imu.readMag()
-	(acc_x, acc_y, acc_z) = imu.readAccel()
+  (mag_x, mag_y, mag_z) = imu.readMag()
+  (acc_x, acc_y, acc_z) = imu.readAccel()
 
-	# Normalising the accelerometer data
-	# Dividing variable (don't know why I use this)
-	acc_norm_div = math.sqrt(acc_x**2 + acc_y**2 + acc_z**2)
+  # Normalising the accelerometer data
+  # Dividing variable (don't know why I use this)
+  acc_norm_div = math.sqrt(acc_x**2 + acc_y**2 + acc_z**2)
 
-	# Normalised values
-	acc_x_norm = acc_x / acc_norm_div
-	acc_y_norm = acc_y / acc_norm_div
+  # Normalised values
+  acc_x_norm = acc_x / acc_norm_div
+  acc_y_norm = acc_y / acc_norm_div
 
-	# Calc pitch and roll using trig
-	pitch = math.asin(acc_x_norm)
+  # Calc pitch and roll using trig
+  pitch = math.asin(acc_x_norm)
 
-    return pitch
+  return pitch
 
 def roll():
-    (mag_x, mag_y, mag_z) = imu.readMag()
-	(acc_x, acc_y, acc_z) = imu.readAccel()
+  (mag_x, mag_y, mag_z) = imu.readMag()
+  (acc_x, acc_y, acc_z) = imu.readAccel()
 
-	# Normalising the accelerometer data
-	# Dividing variable (don't know why I use this)
-	acc_norm_div = math.sqrt(acc_x**2 + acc_y**2 + acc_z**2)
+  # Normalising the accelerometer data
+  # Dividing variable (don't know why I use this)
+  acc_norm_div = math.sqrt(acc_x**2 + acc_y**2 + acc_z**2)
 
-	# Normalised values
-	acc_x_norm = acc_x / acc_norm_div
-	acc_y_norm = acc_y / acc_norm_div
+  # Normalised values
+  acc_x_norm = acc_x / acc_norm_div
+  acc_y_norm = acc_y / acc_norm_div
 
-	# Calc pitch and roll using trig#
-    pitch = math.asin(acc_x_norm)
-	roll = - math.asin(math.radians(acc_y_norm / math.cos(pitch)))
+  # Calc pitch and roll using trig#
+  pitch = math.asin(acc_x_norm)
+  roll = - math.asin(math.radians(acc_y_norm / math.cos(pitch)))
 
-    return roll
+  return roll
 
 def getRollBias():
-    rollAngle = roll() - rollCalibration
-    bias = (rollAngle / 180) + 0.5
+  rollAngle = roll() - rollCalibration
+  bias = (rollAngle / 180) + 0.5
 
-    return bias
+  return bias
 
 def getPitchBias():
-    pitchAngle = pitch() - pitchCalibration
-    bias = (rollAngle / 180) + 0.5
+  pitchAngle = pitch() - pitchCalibration
+  bias = (rollAngle / 180) + 0.5
 
-    return bias
+  return bias
 
 totalPower = 0.1
 rollCalibration = roll()
 pitchCalibration = pitch()
 
 while(True):
-    pitchBias = getPitchBias()
-    rollBias = getRollBias()
-    frontPower = totalPower * pitchBias
-    rearPower = totalPower * (1 - pitchBias)
+  pitchBias = getPitchBias()
+  rollBias = getRollBias()
+  frontPower = totalPower * pitchBias
+  rearPower = totalPower * (1 - pitchBias)
 
-    frontLeftMotor.value = frontPower * rollBias
-    frontRightMotor.value = frontPower * (1 - rollBias)
+  frontLeftMotor.value = frontPower * rollBias
+  frontRightMotor.value = frontPower * (1 - rollBias)
 
-    rearLeftMotor.value = rearPower * rollBias
-    rearRightMotor.value = rearPower * (1 - rollBias)
+  rearLeftMotor.value = rearPower * rollBias
+  rearRightMotor.value = rearPower * (1 - rollBias)
 
-    sleep(0.05)
+  sleep(0.05)
 
 
 
