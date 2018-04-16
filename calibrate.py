afebd1326bfac26711a27941ba187a6240b8fa13
Rob Willison <Rob@willison.co.uk>
2018-04-16 19:00:59 +0100

    more work

--- calibrate.py
+++ calibrate.py
@@ -1,2 +1,21 @@
 import MotorDrive
+from time import sleep
 
+MotorDrive.setup()
+
+mFrontLeft = MotorDrive.MOTOR1
+mFrontRight = MotorDrive.MOTOR2
+mBackLeft = MotorDrive.MOTOR3
+mBackRight = MotorDrive.MOTOR4
+
+def calibrate(motor):
+    motor.setMotorValue(0)
+    sleep(2)
+    motor.setMotorValue(100)
+    sleep(2)
+    motor.setMotorValue(0)
+
+calibrate(mFrontRight)
+calibrate(mFrontLeft)
+calibrate(mBackRight)
+calibrate(mBackLeft)
